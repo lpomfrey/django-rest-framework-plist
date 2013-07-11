cbedfe8b2331a9df6b9085ad502d92fd2cf6e4d5
Luke Pomfrey <luke@lukepomfrey.org>
2013-07-11 16:40:21 +0100

    Bump version to dev version

--- rest_framework_plist/__init__.py
+++ rest_framework_plist/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from distutils import version
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 version_info = version.StrictVersion(__version__).version
 
 from .parsers import PlistParser  # NOQA
