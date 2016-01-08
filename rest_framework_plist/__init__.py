16bb8243bd192e2fae0933f61e6ca07f0d702bb7
Luke Pomfrey <luke.pomfrey@titanemail.com>
2016-01-08 14:36:51 +0000

    Tag versions with plain version number

--- rest_framework_plist/__init__.py
+++ rest_framework_plist/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from distutils import version
 
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 version_info = version.StrictVersion(__version__).version
 
 from rest_framework_plist.parsers import PlistParser  # NOQA
